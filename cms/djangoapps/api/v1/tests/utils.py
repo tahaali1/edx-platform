c61a2778e2117c0b49cc520177d749b7e9c16de9
usamasadiq <usama.sadiq@arbisoft.com>
2021-02-02 14:54:00 +0500

    Applied pylint-amnesty

--- cms/djangoapps/api/v1/tests/utils.py
+++ cms/djangoapps/api/v1/tests/utils.py
@@ -1,2 +1,2 @@
-def serialize_datetime(d):
+def serialize_datetime(d):  # lint-amnesty, pylint: disable=missing-module-docstring
     return d.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
--- common/lib/symmath/symmath/__init__.py
+++ common/lib/symmath/symmath/__init__.py
@@ -1,2 +1,2 @@
-from .formula import *
+from .formula import *  # lint-amnesty, pylint: disable=django-not-configured, missing-module-docstring
 from .symmath_check import *
